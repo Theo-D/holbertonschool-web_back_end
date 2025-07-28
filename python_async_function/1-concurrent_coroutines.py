dc7a67a187a847d89c56f65d24b8d8da04d12dbf
Theo-D <theodessaigne@hotmail.fr>
2025-07-28 15:19:27 +0200

    Added 1-concurrent_coroutines.py that execute tasks by order of execution time

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+1-concurrent_coroutines.py: Contains definition of wait_n().
+"""
+from typing import List, Awaitable, Callable
+from asyncio import as_completed
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    A function that return the list of all the delays (float values).
+    The list of the delays should be in ascending order without using
+    sort() because of concurrency.
+    """
+    wait_random: Callable[[int], Awaitable[float]] = __import__('0-basic_async_syntax').wait_random
+    delay_list: List[float] = []
+
+    tasks: List[Awaitable[float]] = [wait_random(max_delay) for _ in range(n)]
+
+    for task in as_completed(tasks):
+        delay: float = await task
+        delay_list.append(delay)
+
+    return delay_list
