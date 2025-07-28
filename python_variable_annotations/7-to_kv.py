d99ec1911043d2ab4fef103bd105672584178c6f
Theo-D <theodessaigne@hotmail.fr>
2025-07-28 11:55:39 +0200

    Added Union

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,14 +3,14 @@
 7-to_kv.py: Contains to_kv()
 """
 
-from typing import Tuple
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: int | float) -> Tuple[str, int | float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
     """
     A type-annotated function to_kv that takes a string k and
     an int OR float v as arguments and returns a tuple.
     """
     v_squared: float = v * v
-    new_tuple: Tuple[str, int | float] = (k, v_squared)
+    new_tuple: Tuple[str, Union[int, float]] = (k, v_squared)
     return new_tuple
