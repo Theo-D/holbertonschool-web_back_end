87d07e6e2ca61c98fca007d05f168625b30df0a2
Theo-D <theodessaigne@hotmail.fr>
2025-07-28 11:57:05 +0200

    changed content of tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -6,11 +6,11 @@
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     A type-annotated function to_kv that takes a string k and
     an int OR float v as arguments and returns a tuple.
     """
     v_squared: float = v * v
-    new_tuple: Tuple[str, Union[int, float]] = (k, v_squared)
+    new_tuple: Tuple[str, float] = (k, v_squared)
     return new_tuple
