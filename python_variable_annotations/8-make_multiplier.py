3dae245c95211f0bd915db4a86d280ffe5c23b82
Theo-D <theodessaigne@hotmail.fr>
2025-07-28 12:00:23 +0200

    fixed typo on multiplier

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -6,12 +6,12 @@
 from typing import Callable
 
 
-def make_multiplier(mutiplier: float) -> Callable[[float], float]:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
     A type-annotated function make_multiplier that takes
     a float multiplier as argument and returns a function that multiplies
     a float by multiplier.
     """
     def mult_func(multiplier2: float) -> float:
-        return mutiplier * multiplier2
+        return multiplier * multiplier2
     return mult_func
