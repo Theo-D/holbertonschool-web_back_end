2579b7ce5c46696f5d708e999a72197d63d9f6b0
Theo-D <theodessaigne@hotmail.fr>
2025-07-28 23:34:06 +0200

    Added 2-measure-runtime.py as well as a README.md file containing description for the files in the directory.

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+2-measure_runtime: Contains defiunition for measure_runtime().
+"""
+from asyncio import gather
+from time import perf_counter
+
+
+async def measure_runtime():
+    """
+    A coroutine that will execute `async_comprehension` 4 times,
+    measure the total runtime and return it.
+    """
+    async_comprehension = __import__(
+        '1-async_comprehension'
+        ).async_comprehension
+    start = perf_counter()
+
+    await gather(*(async_comprehension() for _ in range(4)))
+
+    end = perf_counter() - start
+
+    return end
--- python_async_comprehension/README.md
+++ python_async_comprehension/README.md
@@ -2,3 +2,7 @@
 ## This directory contains files needed for the project Python - Async Comprehension
 
 0-async_generator.py - A coroutine that will loop 10 times, each time asynchronously wait 1 second, then yield a random number between 0 and 10. Use the random module.
+
+1-async_comprehension.py - A coroutine that will collect 10 random numbers using an async comprehensing over async_generator, then return the 10 random numbers.
+
+2-measure_runtime.py - A coroutine that will execute `async_comprehension` 4 times, measure the total runtime and return it.
