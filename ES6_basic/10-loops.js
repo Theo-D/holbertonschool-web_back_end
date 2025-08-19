7cbba3d82ce5c1f19f696a9bb3d48d1a38c54ea8
Theo-D <theodessaigne@hotmail.fr>
2025-08-19 14:18:03 +0200

    Added task files from 2 to 12

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  let i = 0
+  for (let val of array) {
+    array[i] = appendString + val;
+    i++
+  }
+
+  return array;
+}
---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,8 @@
+export default function createReportObject(employeesList) {
+  const object = {
+    allEmployees: employeesList,
+    getNumberOfDepartments: (departments) => {return Object.keys(departments).length}
+  };
+
+  return object;
+}
