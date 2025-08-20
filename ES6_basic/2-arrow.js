b35120cb2ffe13177ff8f1aee5dbde067e25c1e3
Theo-D <theodessaigne@hotmail.fr>
2025-08-20 10:03:35 +0200

    Chnaged function name

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,8 +1,7 @@
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
   const self = this;
-  let add = (newNeighborhood) => {self.sanFranciscoNeighborhoods.push(newNeighborhood)
+  this.addNeighborhood = (newNeighborhood) => {self.sanFranciscoNeighborhoods.push(newNeighborhood)
     return self.sanFranciscoNeighborhoods;
   };
-  this.addNeighborhood = add;
 }
