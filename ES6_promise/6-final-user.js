1ee90e1fe6d8bcf7227eb9dff0c792886f8d9313
Theo-D <theodessaigne@hotmail.fr>
2025-08-26 17:22:50 +0200

    Added return statement at the end of the function

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,16 +1,14 @@
 import signUpUser from "./4-user-promise";
 import uploadPhoto from "./5-photo-reject";
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   const content = Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
 
-  return content
-  .then((result) => {
-    [
-      {
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : String(result.reason)
-      }
-    ]
-  });
+  const result = await content;
+  result.map((res) => ({
+    status: res.status,
+    value: res.status === 'fulfilled' ? res.value : String(res.reason),
+  }));
+
+  return result;
 }
