8e7a35a0d15b3313282a47a3dd3ab091fbaba452
Theo-D <theodessaigne@hotmail.fr>
2025-08-26 10:17:27 +0200

    Changed return format

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,11 +1,8 @@
 export default function getListStudentIds (studentsArr) {
-  const newArr = Array();
 
   if (!Array.isArray(studentsArr)) {
-    return newArr;
+    return [];
   }
 
-  newArr = studentsArr.map(student => student.id);
-
-  return newArr;
+  return studentsArr.map(student => student.id);
 }
