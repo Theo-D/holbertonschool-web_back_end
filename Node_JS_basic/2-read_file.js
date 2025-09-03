078705e40b381a49b2610cd4a0dc1e5611ff04f5
Theo-D <theodessaigne@hotmail.fr>
2025-09-03 13:55:53 +0200

    Fixed code to conform to eslint

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,41 +1,40 @@
-function countStudents(path) {
-  const fs = require('node:fs');
+const fs = require('node:fs');
 
+function countStudents(path) {
+  let data = '';
   try {
     data = fs.readFileSync(path, 'utf8');
   } catch (err) {
     throw new Error('Cannot load the database');
   }
 
-    const dataArr = data.split('\n');
-    const studentArr = dataArr.filter(elm => elm !== dataArr[0] && elm !== '');
-    const studentKeys = dataArr[0].split(',');
+  const dataArr = data.split('\n');
+  const studentArr = dataArr.filter((elm) => elm !== dataArr[0] && elm !== '');
+  const studentKeys = dataArr[0].split(',');
 
+  const studentDict = studentArr.map((line) => {
+    const object = line.split(',');
+    return studentKeys.reduce((accumulator, key, index) => ({
+      ...accumulator,
+      [key]: object[index],
+    }), {});
+  });
 
-    const studentDict = studentArr
-    .map(line => {
-      const object = line.split(',');
-      return studentKeys.reduce((obj, h, i) => {
-        obj[h] = object[i];
-        return obj;
-      }, {});
-    });
-
-    const byField = studentDict.reduce((accumulator, student) => {
-      const field = student.field;
-      const firstname = student.firstname;
-      if(!accumulator[field]) {
-        accumulator[field] = [];
-      }
-      accumulator[field].push(firstname);
-      return accumulator;
-    }, {});
-
-    console.log(`Number of students: ${studentArr.length}`);
-
-    for (const [key, value] of Object.entries(byField)) {
-      console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
+  const byField = studentDict.reduce((accumulator, student) => {
+    const { field } = student;
+    const { firstname } = student;
+    if (!accumulator[field]) {
+      accumulator[field] = [];
     }
-  };
+    accumulator[field].push(firstname);
+    return accumulator;
+  }, {});
+
+  console.log(`Number of students: ${studentArr.length}`);
+
+  for (const [key, value] of Object.entries(byField)) {
+    console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
+  }
+}
 
 module.exports = countStudents;
