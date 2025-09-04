e9d359f854520d9ee80c473a878c7625f6e900b7
Theo-D <theodessaigne@hotmail.fr>
2025-09-04 09:29:54 +0200

    Changed method for resolving promise

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,43 +1,45 @@
-const fs = require('node:fs/promises');
+const fs = require('fs');
 
-async function countStudents(path) {
-  let output = '';
-  let data = '';
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
 
-  try {
-    data = await fs.readFile(path, {encoding: 'utf8'});
-  } catch (err) {
-    throw new Error('Cannot load the database');
-  }
+      let output = '';
+      const dataArr = data.split('\n');
+      const header = dataArr[0];
+      const studentArr = dataArr.slice(1).filter((line) => line.trim() !== '');
 
-  const dataArr = data.split('\n');
-  const studentArr = dataArr.filter((elm) => elm !== dataArr[0] && elm !== '');
-  const studentKeys = dataArr[0].split(',');
+      const studentKeys = header.split(',');
 
-  const studentDict = studentArr.map((line) => {
-    const object = line.split(',');
-    return Object.fromEntries(
-      studentKeys.map((key, index) => [key, object[index]]),
-    );
+      const studentDict = studentArr.map((line) => {
+        const values = line.split(',');
+        return Object.fromEntries(
+          studentKeys.map((key, index) => [key.trim(), values[index].trim()])
+        );
+      });
+
+      const byField = studentDict.reduce((acc, student) => {
+        const { field, firstname } = student;
+        if (!acc[field]) {
+          acc[field] = [];
+        }
+        acc[field].push(firstname);
+        return acc;
+      }, {});
+
+      output = `Number of students: ${studentArr.length}`;
+      for (const [field, names] of Object.entries(byField)) {
+        output += `\nNumber of students in ${field}: ${names.length}. List: ${names.join(', ')}`;
+      }
+
+      console.log(output);
+      resolve(output);
+    });
   });
-
-  const byField = studentDict.reduce((accumulator, student) => {
-    const { field } = student;
-    const { firstname } = student;
-    if (!accumulator[field]) {
-      accumulator[field] = [];
-    }
-    accumulator[field].push(firstname);
-    return accumulator;
-  }, {});
-
-  output = `Number of students: ${studentArr.length}`;
-  for (const [key, value] of Object.entries(byField)) {
-    output += `\nNumber of students in ${key}: ${value.length}. List: ${value.join(', ')}`;
-  }
-
-  console.log(output);
-  return output;
 }
 
 module.exports = countStudents;
