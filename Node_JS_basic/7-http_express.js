f73ddaf557bdbab160eedf4fcd18188ba4c47b72
Theo-D <theodessaigne@hotmail.fr>
2025-09-04 11:37:05 +0200

    Fixed code to conform to eslint

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const countStudents = require('./3-read_file_async');
+
 const app = express();
 const port = process.env.PORT || 1245;
 const db = process.argv[2];
 
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
-    console.log(db);
-    countStudents(db)
-      .then((data) => res.send(`This is the list of our students\n${data}`))
-      .catch((err) => res.send(`This is the list of our students\n${err.message}`));
+  console.log(db);
+  countStudents(db)
+    .then((data) => res.send(`This is the list of our students\n${data}`))
+    .catch((err) => res.send(`This is the list of our students\n${err.message}`));
 });
 
 app.listen(port, () => {
-    console.log('Server app listening on port ' + port);
+  console.log(`Server app listening on port ${port}`);
 });
 
 module.exports = app;
