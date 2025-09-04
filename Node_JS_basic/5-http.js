de94145d4ec04c800f560a8091686a1341ff2730
Theo-D <theodessaigne@hotmail.fr>
2025-09-04 11:43:00 +0200

    Removed async keyword from create server

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,32 +1,34 @@
 const { createServer } = require('node:http');
 const countStudents = require('./3-read_file_async');
 
+const hostname = '127.0.0.1';
 const port = 1245;
-const db = process.argv[2];
-const app = createServer(async (req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
-
+const app = createServer((req, res) => {
   if (req.url === '/' && req.method === 'GET') {
     res.statusCode = 200;
     res.end('Hello Holberton School!');
-
     return;
   }
 
   if (req.url === '/students' && req.method === 'GET') {
-    try {
-      res.statusCode = 200;
-      const output = await countStudents(db);
-      res.end(`This is the list of our students\n${output}`);
-    } catch (err) {
-      res.statusCode = 404;
-      res.end('Cannot load the database');
-    }
-
+    countStudents('database.csv')
+      .then((output) => {
+        res.statusCode = 200;
+        res.end(`This is the list of our students\n${output}`);
+      })
+      .catch((err) => {
+        res.statusCode = 404;
+        res.end('Cannot load the database');
+      });
     return;
   }
+
+  res.statusCode = 404;
+  res.end('Not Found');
 });
 
-app.listen(port);
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
 
 module.exports = app;
