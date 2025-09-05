3004e72270f11ffd20b05708c7c64888f047395b
Theo-D <theodessaigne@hotmail.fr>
2025-09-05 14:06:44 +0200

    Added full_server with up to date tree structure and utils.js and appcontroller.js done

---
+++ Node_JS_basic/full_server/test_utils.js
@@ -0,0 +1,3 @@
+import readDatabase from './utils.js';
+
+console.log(await readDatabase('../database.csv'));
---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,35 @@
+import fs from 'node:fs';
+
+export default function readDatabase (dbPath) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(dbPath, 'utf8', (err, data) => {
+              if (err) {
+                reject(new Error('Cannot load the database'));
+                return;
+              }
+
+              const dataArr = data.split('\n');
+              const header = dataArr[0];
+              const studentKeys = header.split(',');
+              const studentArr = dataArr.slice(1).filter((line) => line.trim() !== '');
+
+              const studentDict = studentArr.map((line) => {
+                const values = line.split(',');
+                return Object.fromEntries(
+                  studentKeys.map((key, index) => [key.trim(), values[index].trim()]),
+                );
+              });
+
+              const byField = studentDict.reduce((acc, student) => {
+                const { field, firstname } = student;
+                if (!acc[field]) {
+                  acc[field] = [];
+                }
+                acc[field].push(firstname);
+                return acc;
+              }, {});
+
+              resolve(byField);
+        });
+    });
+}
